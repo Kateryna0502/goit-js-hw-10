924d386778cb743755fdfbf972d8bda72073f3e3
Kateryna <mweoliss74@gmail.com>
2024-06-14 17:05:00 +0300

    z

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,91 +1,103 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
-import imageUrl from '../img/symbol-defs.svg';
+import 'izitoast/dist/css/iziToast.css';
 
+let userDate;
+
+let timerInterval;
 const refs = {
-  dateTime: document.querySelector('#datetime-picker'),
-  button: document.querySelector('[data-start]'),
-  timer: {
-    dataDays: document.querySelector('[data-days]'),
-    dataHours: document.querySelector('[data-hours]'),
-    dataMinutes: document.querySelector('[data-minutes]'),
-    dataSeconds: document.querySelector('[data-seconds]'),
-  },
+  picker: document.querySelector('#datetime-picker'),
+  dataStart: document.querySelector('[data-start]'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
 };
-let userSelectedDate;
-let initTimeId;
-refs.button.disabled = true;
+
 flatpickr('#datetime-picker', {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    if (selectedDates[0] < Date.now()) {
-      iziToast.show({
-        title: 'ERROR',
-        message: 'Please choose a date in the future',
-        backgroundColor: '#ef4040',
-        titleColor: '#fff',
-        titleSize: '16px',
-        messageColor: '#fff',
-        messageSize: '16px',
-        progressBarColor: '#B51B1B',
-        position: 'topRight',
-        iconUrl: imageUrl,
-        iconColor: '#FAFAFB',
-        imageWidth: 302,
-        theme: 'dark',
-      });
-    } else {
-      userSelectedDate = selectedDates[0];
-      refs.button.disabled = false;
-    }
+    userSet(selectedDates[0]);
   },
 });
-refs.button.addEventListener('click', () => {
-  refs.button.disabled = true;
-  refs.dateTime.disabled = true;
-  initTimeId = setInterval(() => {
-    const currentTime = Date.now();
+function timer() {
+  refs.dataStart.setAttribute('disabled', '');
+  refs.picker.setAttribute('disabled', '');
+  const novDate = new Date();
 
-    const diff = userSelectedDate - currentTime;
-    if (diff > 0) {
-      const time = convertMs(diff);
-      const str = getTime(time);
-    } else {
-      clearInterval(initTimeId);
+  let intervalId;
+  let timerInterval = userDate - novDate;
+
+  intervalId = setInterval(() => {
+    timerWriter(convertMs(timerInterval));
+    timerInterval -= 1000;
+    if (timerInterval <= 0) {
+      clearInterval(intervalId);
+      stop();
     }
+    console.log(timerInterval);
   }, 1000);
-});
+}
+
+function userSet(userData) {
+  const novDate = new Date();
+
+  if (novDate > userData) {
+    console.log('Future time');
+
+    iziToast.error({
+      title: 'Error',
+      message: 'Illegal operation',
+    });
+    stop();
+    return;
+  }
+  start(userData);
+}
+function start(userData) {
+  refs.dataStart.removeAttribute('disabled');
+  refs.dataStart.addEventListener('click', timer);
+  userDate = userData;
+}
+
+function stop() {
+  refs.dataStart.removeEventListener('click', timer);
+  refs.dataStart.setAttribute('disabled', '');
+  refs.picker.removeAttribute('disabled');
+  userDate = null;
+}
+
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
+
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
+
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
+
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
 }
-function getTime({ days, hours, minutes, seconds }) {
-  days = days.toString().padStart(2, '0');
-  hours = hours.toString().padStart(2, '0');
-  minutes = minutes.toString().padStart(2, '0');
-  seconds = seconds.toString().padStart(2, '0');
 
-  return `${(refs.timer.dataDays.textContent =
-    days)},${(refs.timer.dataHours.textContent =
-    hours)}:${(refs.timer.dataMinutes.textContent =
-    minutes)}:${(refs.timer.dataSeconds.textContent = seconds)}`;
+function timerWriter(time) {
+  console.log(time);
+  console.log(time.seconds);
+  let days = time.days;
+  let hours = time.hours;
+  let minutes = time.minutes;
+  let seconds = time.seconds;
+
+  refs.days.textContent = days.toString().padStart(2, '0');
+  refs.hours.textContent = hours.toString().padStart(2, '0');
+  refs.minutes.textContent = minutes.toString().padStart(2, '0');
+  refs.seconds.textContent = seconds.toString().padStart(2, '0');
 }
